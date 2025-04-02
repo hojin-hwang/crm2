9828c3763923dc20cb7906816b8bfe190d6339cb
Jake Hwang <hojinh@gmail.com>
2025-04-02 15:05:51 +0900

    주석 제거

--- routes/crm.js
+++ routes/crm.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-const clientController = require('../controllers/clientController');
-
 router.get('/:clientId',  async(request,response)=>{
    if(!request.user){
       response.redirect('/user/login/'+request.params.clientId);
@@ -10,17 +8,6 @@ router.get('/:clientId',  async(request,response)=>{
 	}
 
 	response.render('crm.ejs', {"userInfo":request.user, "clientId":request.params.clientId});
-	// clientController.get(request, response).then(data=>{
-	// 	response.render('crm.ejs', {"userInfo":request.user, "clientId":req.params.clientId});
-	// });
-	
-	// clientController.get(request, response).then(data=>{
-	// 	if(data)
-	// 	{
-	// 		response.render('crm.ejs', {"userInfo":request.user, "clientInfo":data});
-	// 	}
-	// 	////response.render('crm.ejs', {"userInfo":request.user, "clientInfo":data});
-	// })
 });
 
 
